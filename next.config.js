043bdb3d7a704844a3a2b1474a87417bba1a6d92
Brandon Luffman <brandonluff10@gmail.com>
2024-05-10 19:41:06 -0400

    added heroku api

--- next.config.js
+++ next.config.js
@@ -1,31 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
-      },
-    ];
-  },
-};
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8000/api/:path*"
+//             : "/api/",
+//       },
+//       {
+//         source: "/docs",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8000/docs"
+//             : "/api/docs",
+//       },
+//       {
+//         source: "/openapi.json",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8000/openapi.json"
+//             : "/api/openapi.json",
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
 
